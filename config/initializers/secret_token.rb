ab19a2267e0d46a8dd9c0d37e588f9363ab32c66
lhex <xiurenyap@gmail.com>
2013-07-26 21:52:36 +0800

    first commit with default gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Singstack::Application.config.secret_token = 'ad69f71a98c7588decdd0d32c43bb3a025e6f3ba82f501c2eec12f26ac6ee66b14676685fdb91374e2afc4ffbfe1238c44a32c95352d7e48be528d9c507942c5'
